c9be2992b4def92159fb842cb535b77987834df9
Efriem Desalew <efidesalew@gmail.com>
2022-11-30 18:55:43 +0300

    Lab01-01- Hello World

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+/*
+ * Debre Berhan University
+ * College of Engineering
+ * Department of Electrical and Computer Engineering
+ *
+ * Introuduction to Computing 
+ * Lab0101 
+ * Description: write a cpp program which can display \"Hello world\" on your terminal
+ */
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"Hello, World!"<<endl;
+	return 0;
+}
